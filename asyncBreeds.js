412fcd78912895bf6f2c1e1bd57a93b7af442c71
Phat Luong <luongtanphat25@gmail.com>
2023-06-11 18:05:44 -0400

    finished async test

---
+++ asyncBreeds.js
@@ -0,0 +1,22 @@
+// asyncBreeds.js
+const fs = require('fs');
+
+const breedDetailsFromFile = function (breed, functionToRunWhenThingsAreDone) {
+  // console.log('breedDetailsFromFile: Calling readFile...');
+  fs.readFile(`./data/${breed}.txt`, 'utf8', (error, data) => {
+    // console.log("In readFile's Callback: it has the data.");
+    if (error) {
+      functionToRunWhenThingsAreDone(undefined);
+      return;
+    }
+    functionToRunWhenThingsAreDone(data);
+  });
+};
+
+// const printOutCatBreed = breed => {
+//   console.log('Return Value: ', breed);// => print out details correctly.
+// };
+
+// breedDetailsFromFile('Bombay', printOutCatBreed);
+
+module.exports = breedDetailsFromFile;
